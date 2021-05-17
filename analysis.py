aacd4b04614dcb56f91c2e4b53701b48b8bed7c7
cmahn <mail@cmahn.de>
2021-05-17 13:51:59 +0200

    Initial files

---
+++ analysis.py
@@ -0,0 +1,15 @@
+# Import von Bibliotheken
+# -----------------------------------------------------------------------------
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math as m
+
+# Funktionen
+# -----------------------------------------------------------------------------
+
+# Haupt-Programm
+# -----------------------------------------------------------------------------
+
+if(__name__ == '__main__'):
+    print("Hello world!")
